177b0b9760b23c6ad5bce83003f1a2a62151bc77
mg <m-aggi.e@hotmail.com>
2018-11-02 17:48:57 -0600

    Cambios

---
+++ Formsa1/Formsa1/LogicaFormas.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Formsa1
+{
+        public  class LogicaFormas
+        {
+        public string RealizarOperacion (string LadoOne, string LadoTwo, string LadoThree, string elOperador)
+        {
+            double LadoUno = Convert.ToDouble(LadoOne); 
+
+        }
+    }
+}
